3e951af0462019afab5df9f74369e918c506e3cc
Mirza MS Baig <rock.baig555@gmail.com>
2024-05-21 07:44:47 -0500

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Hero = () => {
   return (
-    <main className="text-center pb-[135px] pt-[150px] bg-[#e9f5f0]">
+    <main className="text-center py-[140px] bg-[#e9f5f0]">
       <div className="mb-4 ">
         <span className="flex items-center justify-center text-xs text-gray-400 mb-2">
           Powered by
