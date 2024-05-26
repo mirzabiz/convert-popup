f2c0edfb67704ce5f36e3f9260af796962d7833e
Mirza MS Baig <rock.baig555@gmail.com>
2024-05-26 15:52:02 -0500

    bug for empty space

--- components/AppsumoDialog.js
+++ components/AppsumoDialog.js
@@ -30,6 +30,12 @@ const AppsumoDialog = ({ isOpen, onClose }) => {
       toast.error("No user is signed in");
       return;
     }
+    const trimmedCode = code.trim();
+
+    if (trimmedCode === "") {
+      toast.error("Code cannot be empty");
+      return;
+    }
 
     if (!codes.includes(code)) {
       toast.error("Invalid code");
@@ -51,45 +57,45 @@ const AppsumoDialog = ({ isOpen, onClose }) => {
       toast.success("Code redeemed successfully");
 
       window.location.assign('/home');
-  } catch (error) {
-    console.error("Error redeeming code: ", error);
-    toast.error("Error redeeming code");
-  }
-};
+    } catch (error) {
+      console.error("Error redeeming code: ", error);
+      toast.error("Error redeeming code");
+    }
+  };
 
-return (
-  isOpen && (
-    <Dialog open={isOpen} onClose={onClose} className="fixed inset-0 z-[10000] bg-black bg-opacity-60 overflow-y-auto">
-      <div className="flex items-center justify-center min-h-screen">
-        <Dialog.Panel className="px-6 py-4 bg-white rounded shadow">
-          <Dialog.Title className="text-lg font-bold text-center">
-            Redeem your Appsumo Code
-          </Dialog.Title>
-          <Dialog.Description className="mt-2 text-gray-500 text-center">
-            Please enter your Appsumo code to redeem it.
-          </Dialog.Description>
-          <div className="mt-4">
-            <input
-              type="text"
-              placeholder="Enter your Appsumo code"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md"
-              value={code}
-              onChange={(e) => setCode(e.target.value)}
-            />
-          </div>
-          <div className="mt-4">
-            <button
-              onClick={handleRedeem}
-              className="w-full py-2 bg-[#02ad78] text-white rounded-md hover:bg-[#029a6b]"
-            >
-              Redeem Code
-            </button>
-          </div>
-        </Dialog.Panel>
-      </div>
-    </Dialog>
-  )
-);
+  return (
+    isOpen && (
+      <Dialog open={isOpen} onClose={onClose} className="fixed inset-0 z-[10000] bg-black bg-opacity-60 overflow-y-auto">
+        <div className="flex items-center justify-center min-h-screen">
+          <Dialog.Panel className="px-6 py-4 bg-white rounded shadow">
+            <Dialog.Title className="text-lg font-bold text-center">
+              Redeem your Appsumo Code
+            </Dialog.Title>
+            <Dialog.Description className="mt-2 text-gray-500 text-center">
+              Please enter your Appsumo code to redeem it.
+            </Dialog.Description>
+            <div className="mt-4">
+              <input
+                type="text"
+                placeholder="Enter your Appsumo code"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md"
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+              />
+            </div>
+            <div className="mt-4">
+              <button
+                onClick={handleRedeem}
+                className="w-full py-2 bg-[#02ad78] text-white rounded-md hover:bg-[#029a6b]"
+              >
+                Redeem Code
+              </button>
+            </div>
+          </Dialog.Panel>
+        </div>
+      </Dialog>
+    )
+  );
 };
 
 export default AppsumoDialog;
